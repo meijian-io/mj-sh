872fb52479278946cf751cb351c70081c173b61f
wangyalong <wangyalong@imeijian.cn>
2020-05-11 16:24:14 +0800

    update mjk8sdebug.sh

--- shells/mjk8sdebug.sh
+++ shells/mjk8sdebug.sh
@@ -6,34 +6,47 @@ cd ${workDir}/..
 # $1 = 应用
 # $2 = 环境，eg：1、2...
 
-appName=$1
-envNum=$2
+envNum=$1
+appNameKey=$2
 podName=""
 
-if [[ ${appName} == "" ]]; then
-    echo "[error] input app name，eg：( ./mjk8sdebug.sh meijian-site 5 )"
+if [[ ${appNameKey} == "" ]]; then
+    echo "[error] input app name，eg：( mjk8sdebug.sh 5 meijian-site )"
     exit 0
 fi
 if [[ ${envNum} == "" ]]; then
-    echo "[error] input test env number，eg： ./mjk8sdebug.sh meijian-site 5"
+    echo "[error] input test env number，eg： mjk8sdebug.sh 5 meijian-site"
     exit 0
 fi
 
-echo "-------- start debug test${envNum} ${appName} -------- "
+echo "-------- start debug test${envNum} ${appNameKey} -------- "
 
 getPodName() {
-    podName=$(kubectl get pod -n meijian-test${envNum} | grep ${appName} | awk '{print $1}')
+    podName=$(kubectl get pod -n meijian-test${envNum} | grep ${appNameKey} | awk '{print $1}')
 }
 getPodName
 
 if [[ ${podName} == "" ]]; then
-    echo "[error] podName is undefined; env=${envNum} app=${appName}"
+    echo "[error] 没有你要找的容器组; env=${envNum} app=${appNameKey}"
     exit 0
+fi
+
+podArray=(${podName// /})
+if [[ ${#podArray[@]} > 1 ]]; then
+    for(( i=0;i<${#podArray[@]};i++)) do
+        echo "$i: ${podArray[i]}";
+    done;
+
+    read -p "请输入你要进入的pod下标，并回车继续。" choose
+    podName=${podArray[${choose}]}
 fi
 
 echo "-------- the pod is ${podName} -------- "
 
+appFullName=${podName%-*}
+appFullName=${appFullName%-*}
+
 # 系统端口  $serverPort  $debugPort
-source ./opt/script/port.sh
+source ./opt/script/port.sh ${appFullName}
 
 kubectl port-forward ${podName} ${debugPort}:9090 -n meijian-test${envNum}
