faff47cc7f857c2d0faa7f400efde3f315ab1041
wangyalong <wangyalong@imeijian.cn>
2020-05-14 18:24:22 +0800

    Redis哨兵模式脚本

---
+++ shells/mjredis-sentinel-clear.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# $1 要删除的key 正则
+# $2 哪个db，默认db=0
+
+pattern=$1
+dbNum=$2
+host=192.168.1.50
+port=6979
+pwd=Mj@tjsaowu@rDp
+
+if [[ ${pattern} == "" ]]; then
+  echo "请输入需要删除的key正则，eg：mjredis-sentinel-clear.sh *userId* 10"
+  exit 0
+fi
+if [[ ${dbNum} == "" ]]; then
+  dbNum=0
+fi
+
+redis-cli -h ${host} -p ${port} -a ${pwd} -n ${dbNum} keys "${pattern}" 2>/dev/null | xargs redis-cli -h ${host} -p ${port} -a ${pwd} -n ${dbNum} del 2>/dev/null
---
+++ shells/mjredis-sentinel-cli.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+envNum=$1
+dbNum=0
+
+if [[ ${envNum} == "" ]]; then
+  echo "eg1：mjredis-sentinel-cli.sh t1（t1环境，db=10...）"
+  echo "eg2：mjredis-sentinel-cli.sh 8（db=8）"
+  dbNum=0
+elif [[ ${envNum} == "t1" ]]; then
+  dbNum=10
+elif [[ ${envNum} == "t2" ]]; then
+  dbNum=20
+elif [[ ${envNum} == "t3" ]]; then
+  dbNum=30
+elif [[ ${envNum} == "t4" ]]; then
+  dbNum=40
+elif [[ ${envNum} == "t5" ]]; then
+  dbNum=50
+elif [[ ${envNum} == "t6" ]]; then
+  dbNum=60
+elif [[ ${envNum} == "t7" ]]; then
+  dbNum=70
+elif [[ ${envNum} == "t8" ]]; then
+  dbNum=80
+elif [[ ${envNum} == "t9" ]]; then
+  dbNum=90
+else
+  echo "eg1：mjredis-sentinel-cli.sh t1（t1环境，db=10...）"
+  echo "eg2：mjredis-sentinel-cli.sh 8（db=8）"
+  dbNum=$1
+fi
+
+redis-cli -h 192.168.1.50 -a Mj@tjsaowu@rDp -p 6979 -n ${dbNum} 2>/dev/null
