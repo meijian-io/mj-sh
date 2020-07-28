9b7882fe5076f2870d67f93ff9291863909a3ded
wangyalong <wangyalong@imeijian.cn>
2020-07-28 15:16:37 +0800

    删除快照包中间增加确认

--- shells/mjmavenDelSnapshot.sh
+++ shells/mjmavenDelSnapshot.sh
@@ -5,6 +5,11 @@ del_package=$1
 get_local_setEnv() {
     mvnRepo=$(mvn help:evaluate -Dexpression=settings.localRepository | grep -v '\[INFO\]')
     sleep 3s
+    echo "M2_LOCAL_REPOSITORY=${mvnRepo}"
+    read -p "确定要添加上面的环境变量吗？y/n。" envAdd
+    if [ ${envAdd} != "y" ]; then
+        exit 0
+    fi
     echo "export M2_LOCAL_REPOSITORY=${mvnRepo}" >> ~/.bash_profile
     source ~/.bash_profile
     if [[ -f "$HOME/.zshrc" ]]; then
@@ -18,6 +23,11 @@ if [[ ${mvnRepo} == "" ]]; then
     get_local_setEnv
 fi
 cd ${mvnRepo}
+
+read -p "确定要删除 [${mvnRepo}] 目录下的 \"*-SNAPSHOT\" 包吗？y/n。" delSanpshot
+if [ ${delSanpshot} != "y" ]; then
+    exit 0
+fi
 
 if [[ ! -n "$del_package" ]]; then
   echo "没有入参------删除所有快照包"
